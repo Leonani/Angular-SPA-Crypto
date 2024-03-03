78dbe70c2387b4f5c82a2087b6a0962b363a0507
LEONEL ESCUDERO <71057664+Leonani@users.noreply.github.com>
2024-03-03 10:24:30 -0300

    Update api.service.ts

--- src/app/service/coinGecko/api.service.ts
+++ src/app/service/coinGecko/api.service.ts
@@ -28,7 +28,7 @@ export class ApiService {
 
   getAllCoins(){
    
-    return this.http.get<ListCoins[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en')
+    return this.http.get<ListCoins[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd')
     
   }
 
