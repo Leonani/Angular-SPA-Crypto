3bab6f8eaccaf8e35070eea15969812d27561856
LEONEL ESCUDERO <71057664+Leonani@users.noreply.github.com>
2024-03-03 10:30:21 -0300

    Update api.service.ts

--- src/app/service/coinGecko/api.service.ts
+++ src/app/service/coinGecko/api.service.ts
@@ -12,32 +12,7 @@ export class ApiService {
   constructor(private http:HttpClient) { }
 
   getAllCoins(){
-   
-    return this.http.get<ListCoins[]>('import { Injectable } from '@angular/core';
-import {ListCoins} from '../../interface/ListCoins.interface'
-import { DetailCoin } from '../../interface/DetailCoin.interface';
-import {HttpClient, HttpHeaders} from '@angular/common/http'
-import {Observable} from 'rxjs'
-
-@Injectable({
-  providedIn: 'root'
-})
-export class ApiService {
- 
-  constructor(private http:HttpClient) { }
-
-  getAllCoins(){
-   
     return this.http.get<ListCoins[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd')
-    
-  }
-
-  getDetail(id: any):Observable <DetailCoin[]>{
-    return this.http.get<DetailCoin[]>( 'https://api.coingecko.com/api/v3/coins/'+ id)
-  }
-
-}')
-    
   }
 
   getDetail(id: any):Observable <DetailCoin[]>{
